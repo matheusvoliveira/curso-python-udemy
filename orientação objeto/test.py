3992b62cd3dae3ff5569294277c8db2253278e0a
Matheus Soares <matheusbraganca321@hotmail.com>
2023-04-03 17:34:50 -0300

    first commit

---
+++ orientação objeto/test.py
@@ -0,0 +1,5 @@
+def criaConta(numero, titular, saldo, limite):
+    conta = {'numero': numero, 'titular': titular, 'saldo': saldo, 'limite': limite}
+    return conta
+conta = criaConta(123, 'Nico', 55.0, 1000.0)
+print(conta['numero'])
