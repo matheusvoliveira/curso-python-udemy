eabb76d40c23bced94062f641629e4430d86a474
Matheus Soares <matheusbraganca321@hotmail.com>
2023-04-20 23:25:05 -0300

    property e setters

--- oo/modelo.py
+++ oo/modelo.py
@@ -1,30 +1,51 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
-    def dar_like(self):
-        self.likes += 1
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_likes(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
 
 class Serie:
-    def __init__(self, nome, ano, temporada):
-        self.nome = nome.title()
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
         self.ano = ano
-        self.temporada = temporada
-        self.likes = 0
+        self.temporadas = temporadas
+        self.__likes = 0
 
-    def dar_like(self):
-        self.likes += 1
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_likes(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-print('Nome:  {}  - Ano:  {}  - Duração: {}  Likes:  {}'.format(vingadores.nome, vingadores.ano, vingadores.duracao,
-                                                             vingadores.likes))
+print(vingadores.nome)
 
-atlanta = Serie('Atlanta', 2017, 4)
-atlanta.dar_like()
-atlanta.dar_like()
-print('Nome:  {}  - Ano:  {}  - Temporadas:  {}  Likes:  {}'.format(atlanta.nome, atlanta.ano, atlanta.temporada,
-                                                                    atlanta.likes))
+atlanta = Serie('atlanta', 2018, 2)
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
