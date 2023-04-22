f4f6c46c491324ce63be11f3095e4fe4c0564ec8
Matheus Soares <matheusbraganca321@hotmail.com>
2023-04-22 01:00:08 -0300

    classe mãe e super()

--- oo/modelo.py
+++ oo/modelo.py
@@ -1,47 +1,38 @@
-class Filme:
+class Programa:
+#uma classe mãe que deixa alguns atributos pre atribuidos para nao ser nescessario atribuir em todas class - Herança
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self.__likes = 0
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_likes(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
+        #super() chama o init da classe mãe com os atributos nome e ano eles não precisam ser declarados de novo.
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
-class Serie:
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.__likes = 0
+        super().__init__(nome, ano)
+        self.temporada = temporadas
 
-    @property
-    def likes(self):
-        return self.__likes
 
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
