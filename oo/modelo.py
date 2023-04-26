90e3e4d5a73e2bd50a7d9f2a02c0fd53c6ac8835
Matheus Soares <matheusbraganca321@hotmail.com>
2023-04-25 23:55:25 -0300

    polimorfismo

--- oo/modelo.py
+++ oo/modelo.py
@@ -4,6 +4,10 @@ class Programa:
         self._nome = nome.title()
         self.ano = ano
         self.__likes = 0
+
+    def imprime(self):
+        print(f'Nome: {self.nome} Likes: {self.likes}')
+
     @property
     def likes(self):
         return self.__likes
@@ -25,23 +29,29 @@ class Filme(Programa):
         #super() chama o init da classe mãe com os atributos nome e ano eles não precisam ser declarados de novo.
         self.duracao = duracao
 
-
+    def imprime(self):
+        print(f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}')
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
-        self.temporada = temporadas
+        self.temporadas = temporadas
 
+    def imprime(self):
+        print(f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}')
 
-
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 # print(vingadores.nome)
-
+vingadores.dar_likes()
 atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_likes()
 # print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}')
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporada
-    print(f'{programa.nome} - {detalhes} - {programa.likes}')
+    programa.imprime()
