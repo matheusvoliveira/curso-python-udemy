10df428c08f7f1797d7db942c2fc78552388a85a
Matheus Soares <matheusbraganca321@hotmail.com>
2023-05-05 16:40:07 -0300

    challenge.py

--- oo/homwork-oo.py
+++ oo/homwork-oo.py
@@ -1,11 +1,12 @@
-from math import sqrt
+from math import sqrt, pi
+
 class Line:
     def __init__(self, coor1, coor2):
         self.coor1 = coor1
         self.coor2 = coor2
 
     def distance(self):
-        formula = self.coor2[1] - self.coor1[1] / sqrt((self.coor2[0] - self.coor1[0])**2 + (cordinate2[1] - cordinate1[1]**2))
+        formula = sqrt((self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2)
         return formula
 
 
@@ -18,9 +19,32 @@ class Line:
 
 
 cordinate1 = (3,2)
-
 cordinate2 = (8,10)
 
 li = Line(cordinate1,cordinate2)
 
 print(li)
+
+
+class Cylinder:
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        formula = pi * self.radius**2 * self.height
+        return formula
+
+    def surface_area(self):
+        formula = (2 * pi * self.radius * self.height) + (2 * pi * self.radius**2)
+        return formula
+
+    def __str__(self):
+        return f'Cylinder height: {self.height}, radius: {self.radius}'
+
+
+c = Cylinder(12,2.5)
+print(c)
+print(f"Volume of the cylinder: {c.volume()}")
+print(f"Surface area of the cylinder: {c.surface_area()}")
