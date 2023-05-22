6bb485ac47642b2b779beced2b3c8f156ba82ae8
Matheus Soares <matheusbraganca321@hotmail.com>
2023-05-21 23:36:09 -0300

    update

---
+++ oo/file.py
@@ -0,0 +1,27 @@
+def hello(name):
+    print('The hello() function has been executed')
+    def greet():
+        print('greet() func')
+
+    def welcome():
+        print('welcome() func')
+
+    if name == 'José':
+        print(greet())
+    else:
+        print(welcome())
+    print('this is the end of hello() func')
+
+hello('José')
+
+
+def new_decorator(original_func):
+    def wrap_func():
+        print('Some extra code, before the original function')
+        original_func()
+        print('Some extra code, after the original function')
+    return wrap_func
+
+
+def func_needs_decorator():
+    print('I want to be decorated')
