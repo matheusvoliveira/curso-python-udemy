1269112636b2d9f04c827230ec040fb8e393200d
Matheus Soares <matheusbraganca321@hotmail.com>
2023-05-29 22:16:50 -0300

    update bot_olx.py

--- bot_olx.py
+++ bot_olx.py
@@ -1,5 +1,3 @@
-from lib2to3.pgen2 import driver
-
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
@@ -9,31 +7,52 @@ from selenium.webdriver.edge.options import Options
 import json
 import pyautogui
 
+
 class Acess:
 
     def __init__(self):
         self.url = 'https://www.olx.com.br/brasil?pe=750&ps=650&q=xbox+one&sf=1&opst=2'
         self.driver = webdriver.Edge()
-    def open_web(self):
-       option = Options()
-       option.headless = True
-       self.driver.get(self.url)
-       #time.sleep(10)
-       pop_up = self.driver.find_element(By.XPATH,
-                                    '//*[@id="cookie-notice-ok-button"]').click()
 
+    def open_web(self):
+        option = Options()
+        option.add_argument('--headless')
+        self.driver.get(self.url)
+        time.sleep(10)
+
+        # counter = 1
+        # max_annoucement = 50
+        # # while counter < max_annoucement:
+        #self.driver.find_element(By.XPATH,
+        #                          f'/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/'
+        #                          f'div[11]/div/div/div/ul/li[{counter}]/div').click()
+        # time.sleep(5)
 
     def click_announcement(self):
+        self.driver.find_element(By.XPATH,
+                                 '//*[@id="cookie-notice-ok-button"]').click()
+        time.sleep(2)
         counter = 1
         max_annoucement = 50
         while counter < max_annoucement:
             self.driver.find_element(By.XPATH,
-                                           f'/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/'
-                                           f'div[11]/div/div/div/ul/li[{counter}]/div').click()
+                                     f'/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/div[11]/div/div/div/ul/li[{counter}]/div').click()
             counter += 1
             print(counter)
+            time.sleep(2)
+            # acha a janela recem aberta e feche ela
+            handles = self.driver.window_handles
+            self.driver.switch_to.window(handles[-1])
 
-        driver.quit()
+
+            self.driver.close()
+            self.driver.switch_to.window(handles[0])
+            time.sleep(10)
+            #self.driver.find_element(By.XPATH,
+                                     #'//*[@id="cookie-notice-ok-button"]').click()
+
+        self.driver.quit()
+
 
 olx_bot = Acess()
 olx_bot.open_web()
