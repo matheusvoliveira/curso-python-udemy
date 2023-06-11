6cccaac7609294ba4e68f7390d2b22227698c8ae
Matheus Soares <matheusbraganca321@hotmail.com>
2023-06-11 20:39:46 -0300

    update my_image.py

--- oo/bot.py
+++ oo/bot.py
@@ -1,14 +1,30 @@
-import scrapy
+import requests
+import bs4
+# import scrapy
+#
+# class Olx():
+#     name = 'olx'
+#     start_url = ['https://www.olx.com.br/estado-mg?q=xbox%20one']
+#
+#     def parse():
+#         for i in response.xpath('/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/div[11]/div/div/div/ul'):
+#             info = i.xpath('/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/div[11]/div/div/div/ul').getall()
+#
+#         print(info)
+#
+# olx = Olx
+# olx.parse()
 
-class Olx():
-    name = 'olx'
-    start_url = ['https://www.olx.com.br/estado-mg?q=xbox%20one']
+def authors():
+    print(8*'-')
+    print('\nauthors\n'.upper())
+    print(8 * '-')
+    pages = 1
+    url = 'https://www.olx.com.br/videogames?q=xbox%20one&op=2&opst=2'
+    response_authors = requests.get(url)
+    soup_authors = bs4.BeautifulSoup(response_authors.text, 'lxml')
+    print(response_authors)
+    authors = soup_authors.select('.sc-1fcmfeb-2.dvcyfD')
+    print(authors)
 
-    def parse():
-        for i in response.xpath('/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/div[11]/div/div/div/ul'):
-            info = i.xpath('/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div/div/div[11]/div/div/div/ul').getall()
-
-        print(info)
-
-olx = Olx
-olx.parse()
+authors()
---
+++ oo/my_images/my_image.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import pdb
+mac = Image.open('example.jpg')
+crop = mac.crop((0,0,50,50))
+# mac.show() mostra a imagem
+pencils = Image.open('pencils.jpg')
+
+
+
+
+
+pdb.set_trace()
+
+
