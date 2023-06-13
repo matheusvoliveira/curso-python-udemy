57a0c35a755fa5ce0aee7830bceda52550d3118c
Matheus Soares <matheusbraganca321@hotmail.com>
2023-06-13 09:59:06 -0300

    update my_pdf.py

--- oo/my_pdf/my_pdf.py
+++ oo/my_pdf/my_pdf.py
@@ -1,1 +1,30 @@
-WGFqef
+import PyPDF4
+import pdb
+
+f = open('Working_Business_Proposal.pdf','rb')
+pdf_reader = PyPDF4.PdfFileReader(f)
+print(pdf_reader.numPages)
+
+page_one = pdf_reader.getPage(0)
+page_one_text = page_one.extractText()
+
+pdf_writer = PyPDF4.PdfFileWriter()
+pdf_writer.addPage(page_one)
+
+pdf_output = open('Some_BrandNew_Doc.pdf', 'wb')
+pdf_writer.write(pdf_output)
+
+f.close()
+pdf_output.close()
+
+f = open('Working_Business_Proposal.pdf','rb')
+
+pdf_text = []
+pdf_reader = PyPDF4.PdfFileReader(f)
+
+for num in range(pdf_reader.numPages):
+    page = pdf_reader.getPage(num)
+    pdf_text.append(page.extractText())
+
+print(pdf_text[1])
+pdb.set_trace()
